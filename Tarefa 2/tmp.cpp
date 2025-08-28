878c1060af7756a53d6789fab69e58cc76a17d8a
VitorLucioOliveira <vitor.lucio.0916@gmail.com>
2025-08-28 19:45:26 -0300

    feito

--- Tarefa 2/tmp.cpp
+++ Tarefa 2/tmp.cpp
@@ -8,10 +8,12 @@ int main()
     #pragma omp parallel num_threads(2) // seta o número de threads em 2 
     {
         int tid = omp_get_thread_num(); // lê o identificador da thread 
+        #pragma omp for ordered schedule(dynamic)// saida em ordem, esperando threds nao completas
         for(i = 1; i <= 3; i++) 
         {
-           printf("[PRINT1] T%d = %d \n",tid,i);
-           printf("[PRINT2] T%d = %d \n",tid,i);
+            #pragma omp ordered
+            printf("[PRINT1] T%d = %d \n",tid,i);
+            printf("[PRINT2] T%d = %d \n",tid,i);
         }
     }
 }
